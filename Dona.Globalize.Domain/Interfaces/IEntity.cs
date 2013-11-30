1ea195630ac2137a5a5e6ba03c798692046935dc
Diego Doná <diedona@gmail.com>
2013-11-30 21:00:20 -0200

    Nova interface para o Domain. Add projeto de infraestrutura

---
+++ Dona.Globalize.Domain/Interfaces/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dona.Globalize.Domain.Interfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+        bool Status { get; set; }
+        DateTime DtInsert { get; set; }
+        DateTime DtLastUpdate { get; set; }
+    }
+}
--- Dona.Globalize.Domain/Product.cs
+++ Dona.Globalize.Domain/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dona.Globalize.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace Dona.Globalize.Domain
 {
-    public class Product
+    public class Product : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public DateTime DtInsert { get; set; }
+        public DateTime DtLastUpdate { get; set; }
         public bool Status { get; set; }
     }
 }
