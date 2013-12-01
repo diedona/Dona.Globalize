0637f2633f189bdc2bdf4755d0ea6310ef5269f6
Diego Doná <diedona@gmail.com>
2013-12-01 00:14:43 -0200

    Layout do index e da tela de criação

--- Dona.Globalize.Web/Controllers/HomeController.cs
+++ Dona.Globalize.Web/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using Dona.Globalize.Infra.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Linq;
+using System.Data.Entity;
 
 namespace Dona.Globalize.Web.Controllers
 {
     public class HomeController : Controller
     {
+        GlobalizeDataContext _db;
+
+        public HomeController()
+        {
+            _db = new GlobalizeDataContext();
+        }
         public ActionResult Index()
+        {
+            return View(_db.Products.OrderBy(x => x.DtInsert).ToList());
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
