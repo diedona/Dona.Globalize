0e34b35ce8d93f9246d23faf78ff2f9317022783
Diego Doná <diedona@gmail.com>
2013-11-30 22:59:46 -0200

    projeto web + arquivos não versionados

---
+++ Dona.Globalize.Infra/DataContext/GlobalizeDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Dona.Globalize.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dona.Globalize.Infra.DataContext
+{
+    public class GlobalizeDataContext : DbContext
+    {
+        public GlobalizeDataContext()
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
